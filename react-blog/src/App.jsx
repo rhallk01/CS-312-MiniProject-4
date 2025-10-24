01e983f681551a755580af11a27b0c8c37a73528
Haley Kloss <hrk85@nau.edu>
2025-10-23 20:43:47 -0700

    reorganized vite template + mini proj 3 files to go together better

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -22,7 +20,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
