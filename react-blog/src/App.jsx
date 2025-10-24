d6d902975fa63ab5260edbe2417de57b350dd594
Haley Kloss <hrk85@nau.edu>
2025-10-23 23:00:11 -0700

    added \'api\' stuff to connect react to index.js

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -5,10 +5,37 @@ import BlogPostForm from './components/Form.jsx'
 import EditPost from './components/EditPost.jsx'
 import SignIn from './components/SignIn.jsx'
 import SignUp from './components/SignUp.jsx'
+import api from './api'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  //set load state to true
+  const fetchPosts = async () => {
+    setLoading(true)
+  }
+
+  //load all posts
+  useEffect(() => {
+    (async () => {
+      try {
+        //set loading state to true
+        setLoading(true)
+        //get request to api to send to index.js to get blog posts
+        const { data } = await api.get('/api/blogs')
+        //set the posts const with either posts retrieved or []
+        setPosts(data || [])
+      } catch (e) {
+        console.error(e)
+      } finally {
+        //set loading state to false
+        setLoading(false)
+      }
+    })()
+  }, [])
 
   return (
     <>
